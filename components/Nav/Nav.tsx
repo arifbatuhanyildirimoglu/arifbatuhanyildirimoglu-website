25814e224abacde9af2c52f5e5cbaf2823e4983f
Arif Batuhan Yıldırımoğlu <arifbatuhanyildirimoglu@hotmail.com>
2024-03-30 14:05:28 +0300

    -Added download CV option to the navbar

--- components/Nav/Nav.tsx
+++ components/Nav/Nav.tsx
@@ -89,6 +89,39 @@ const Nav: React.FC = () => {
                   Contact
                 </Link>
               </motion.li>
+              <motion.li variants={listItem} whileHover={{ scale: 1.1 }}>
+                <Link
+                  href="/files/ArifBatuhanYildirimogluCV.pdf"
+                  target='_blank'
+                  onClick={() => {
+                    if (isMobile) setIsMenuOpen((prev) => !prev);
+                  }}
+                  download={'ArifBatuhanYildirimogluCV.pdf'}
+                  style={{
+                    display: 'flex',
+                    alignItems: 'center',
+                    gap: '0.5rem',
+                  }}
+                >
+                  Download CV
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    width={24}
+                    height={24}
+                    viewBox="0 0 24 24"
+                    strokeWidth={1.5}
+                    stroke="currentColor"
+                    className="w-6 h-6"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      d="M3 16.5v2.25A2.25 2.25 0 0 0 5.25 21h13.5A2.25 2.25 0 0 0 21 18.75V16.5M16.5 12 12 16.5m0 0L7.5 12m4.5 4.5V3"
+                    />
+                  </svg>
+                </Link>
+              </motion.li>
               <motion.li
                 className={styles.listItemX}
                 variants={listItem}
